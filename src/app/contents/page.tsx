1931b0ddc22bc555fda7ff3ff3147a412aadfbbd
kazutan1230 <kazuki.nagai@bnt.benextgroup.jp>
2024-01-18 18:59:37 +0900

    For vercel add.

--- src/app/contents/page.tsx
+++ src/app/contents/page.tsx
@@ -1,18 +1,9 @@
-import dynamic from "next/dynamic";
 import React from "react";
 
 import AframeTest from "../../components/map/aframeTest"
 
 // Mapを呼び出す
 export default function ContentsPage() {
-    const Map = React.useMemo(
-        () =>
-        dynamic(() => import("../../components/map/map"), {
-            loading: () => <p>A content is loading</p>,
-            ssr: false,
-        }),
-        []
-    );
     return (
         <div>
             <AframeTest />
