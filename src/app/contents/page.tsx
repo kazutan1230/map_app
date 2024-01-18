56d12917915fdafa67b5aa8f7813daf587fc6fc9
kazutan1230 <kazuki.nagai@bnt.benextgroup.jp>
2024-01-18 19:56:25 +0900

    contents/page.tsx change.

--- src/app/contents/page.tsx
+++ src/app/contents/page.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+import dynamic from "next/dynamic";
 import AframeTest from "../../components/map/aframeTest"
 
-// Mapを呼び出す
+// Aframeページを呼び出す
 export default function ContentsPage() {
+    const AframeTest = dynamic(() => import("../../components/map/aframeTest"), {
+        loading: () => <p>A content is loading</p>,
+        ssr: false,
+    })
+    
     return (
         <div>
             <AframeTest />
