97a15b23ef0ad0bc77e9e176f3e09b0993803217
kazutan1230 <kazuki.nagai@bnt.benextgroup.jp>
2024-11-15 05:59:44 +0000

    debug: for next15 rule -params&searchParamsの修正

--- app/contents/page.tsx
+++ app/contents/page.tsx
@@ -1,11 +1,16 @@
 import React from "react"
 import ViewVR from "@/components/aframe/viewVR"
 
+type ContentsParams = Promise<{
+    srcType: string
+    src: string
+}>
+// { [key: string]: string | string[] | undefined }
 // ViewVRを呼び出す
 export default async function Contents({
     searchParams,
 }: {
-    searchParams: { [key: string]: string | string[] | undefined }
+    searchParams: ContentsParams
 }) {
     const {srcType, src} = await searchParams
     console.log("srcType: " + srcType)
